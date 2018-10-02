e9b5000e2faa6b27a0e73317aed84588db7390b0
KimMyeongSung <myungsung510@naver.com>
2018-10-02 16:25:51 +0900

    2018-10-02 16:25 깃허브 커밋

---
+++ app/src/main/java/com/example/pc/gittestproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pc.gittestproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**깃허브 연동 테스트 프로젝트*/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
