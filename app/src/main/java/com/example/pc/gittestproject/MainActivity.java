085e8b8402e749abe71a64a48d7296a45104a911
KimMyeongSung <myungsung510@naver.com>
2018-10-04 08:42:07 +0900

    1004테스트

--- app/src/main/java/com/example/pc/gittestproject/MainActivity.java
+++ app/src/main/java/com/example/pc/gittestproject/MainActivity.java
@@ -11,15 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //첫번째 업로드
-        ///////
-
-        ///////////////
-
-
-
-        /////////////////////
-        /*??!*/
-        /***/
+        //삭제
     }
 }
